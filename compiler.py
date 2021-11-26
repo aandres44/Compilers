41859d986545baab8fcccb11ce58dc1caf5b5231
Andres Aguirre <chuy27arts@gmail.com>
2021-11-26 05:19:19 -0600

    cleaning useless code, adding basic expressions

--- compiler.py
+++ compiler.py
@@ -47,19 +47,27 @@ t_FINISH = r';'
 
 
 # Token
-def t_NAME(t):
-    r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = reserved.get(t.value,'NAME')    # Check for reserved words
-    return t
-
-def t_FNUMBER(t):
+def t_FLOAT_VAL(t):
     r'\d+\.\d+'
     t.value = float(t.value)
     return t
 
-def t_INUMBER(t):
+
+def t_INT_VAL(t):
     r'\d+'
     t.value = int(t.value)
+    return t
+
+
+def t_STR_VAL(t):
+    r'\"[^\n]+\"'
+    t.value = t.value.replace("\"", "")
+    return t
+
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t.type = reserved.get(t.value, 'ID')
     return t
 
 def t_newline(t):
@@ -85,95 +93,76 @@ precedence = (
 )
 
 # dictionary of names
-names = {}
-abstractTree = []
+instructions = ()
 
-def p_statement_declare_int(p):
-    '''statement : INTDEC NAME is_assing
-    '''
-    if type(p[3]) == 'float':
-        print('No puedes asignar flotantes a enteros')
-    else:
-        names[p[2]] = { "type": "INT", "value": p[3]}
+def p_start(p):
+    '''start : statement'''
+    global instructions
+    instructions = p[1]
 
-def p_is_assing(p):
-    '''is_assing : "=" expression 
-                | '''
-    p[0] = 0
+
+def p_statement(p):
+    '''statement : print_stmt FINISH statement
+                | register_stmt FINISH statement
+                | condition_stmt statement
+                | for_stmt statement
+                | while_stmt statement
+                | empty'''
     if len(p) > 2:
-        p[0] = p[2]
+        if p[2] == ';':
+            p[2] = p[3]
+        p[0] = (p[1],) + p[2]
+    else:
+        p[0] = ()
 
-def p_statement_declare_float(p):
-    'statement : FLOATDEC NAME'
-    names[p[2]] = { "type": "FLOAT", "value":0}
+def p_expression_operation(p):
+    '''expression : expression AND expression
+                | expression OR expression
+                | expression PLUS expression
+                | expression MINUS expression
+                | expression MULT expression
+                | expression DIV expression
+                | expression EXP expression
+                | expression EQUALS expression
+                | expression NOT_EQUALS expression
+                | expression EQ_MORE expression
+                | expression EQ_LESS expression
+                | expression MORE expression
+                | expression LESS expression'''
+    p[0] = ('operation', p[1], p[2], p[3])
 
-def p_statement_print(p):
-    '''statement : PRINT '(' expression ')' '''
-    print(p[3])
-
-def p_statement_assign(p):
-    'statement : NAME "=" expression'
-    if p[1] not in names:
-        print ( "You must declare a variable before using it")
-    names[p[1]]["value"] = p[3]
-
-
-def p_statement_expr(p):
-    'statement : expression'
-    # print(p[1])
-
-def p_expression_binop(p):
-    '''expression : expression '+' expression
-                  | expression '-' expression
-                  | expression '*' expression
-                  | expression '/' expression'''
-    if p[2] == '+':
-        p[0] = p[1] + p[3]
-    elif p[2] == '-':
-        p[0] = p[1] - p[3]
-
-def p_expression_uminus(p):
-    "expression : '-' expression %prec UMINUS"
-    p[0] = -p[2]
-
-def p_expression_group(p):
-    "expression : '(' expression ')'"
-    p[0] = p[2]
-
-def p_expression_inumber(p):
-    "expression : INUMBER"
+def p_expression_id(p):
+    "expression : ID"
     p[0] = p[1]
 
-def p_expression_fnumber(p):
-    "expression : FNUMBER"
+
+def p_expression_val(p):
+    '''expression : FLOAT_VAL
+                 | INT_VAL
+                 | STR_VAL
+                 | bool_val'''
     p[0] = p[1]
 
-def p_expression_name(p):
-    "expression : NAME"
-    try:
-        p[0] = names[p[1]]["value"]
-    except LookupError:
-        print("Undefined name '%s'" % p[1])
-        p[0] = 0
+
+def p_bool_val(p):
+    '''bool_val : TRUE
+              | FALSE'''
+    if p[1] == "true":
+        p[0] = True
+    elif p[1] == "false":
+        p[0] = False
 
 def p_error(p):
     if p:
         print(p)
-        print("Syntax error at line '%s' character '%s'" % (p.lexpos, p.lineno) )
+        print("Syntax error at '%s'" % p.value)
     else:
         print("Syntax error at EOF")
 
-parser = yacc.yacc()
-
-# Console 
-#while True:
-#    try:
-#        s = input('calc > ')
-#    except EOFError:
-#        break
-#    if not s:
-#        continue
-#    yacc.parse(s)
+yacc.yacc()
+file = open("input.txt", "r")
+s = file.read()
+yacc.parse(s)
 
 #File
 in_data = []
